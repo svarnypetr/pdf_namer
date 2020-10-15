8a847bd6658494585d7d76e685346fb5524ca0a0
svarnypetr <svarnypetr@gmail.com>
2020-10-15 18:49:24 +0200

    implement renaming of pdfs in folder for name-whole_title_pattern

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# pdf_namer
-A tool to name PDFs based on their metadata.
+# PDF namer
+A small script to name PDFs based on their metadata.
+
+## Installation
+* Developed with Python 3.6
+* Install dependencies/Python packages in requirements.txt
+  (e.g., use `pip install -r requirements`)
--- pdf_namer.py
+++ pdf_namer.py
@@ -1,27 +1,46 @@
-# get_doc_info.py
+
+import argparse
+import glob
+import os
 
 from PyPDF2 import PdfFileReader
 
+
 class PdfReader:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.pdf_list = glob.glob(os.path.join(folder_path, '*.pdf'))
 
+    @staticmethod
+    def get_info(pdf_path):
+        path = pdf_path
+        with open(path, 'rb') as f:
+            pdf = PdfFileReader(f)
+            info = pdf.getDocumentInfo()
 
+        author = info.author
+        title = info.title
+        return author, title
 
+    @staticmethod
+    def generate_name(author, title):
+        author_part = author.split(',')[0].split(' ')[-1]
+        title_part = title.replace(' ', '_')
+        return '-'.join([author_part, title_part]) + '.pdf'
 
-def get_info(path):
-    with open(path, 'rb') as f:
-        pdf = PdfFileReader(f)
-        info = pdf.getDocumentInfo()
-        number_of_pages = pdf.getNumPages()
-
-    print(info)
-
-    author = info.author
-    creator = info.creator
-    producer = info.producer
-    subject = info.subject
-    title = info.title
+    def rename_files(self):
+        for pdf_file in self.pdf_list:
+            author, title = self.get_info(pdf_file)
+            new_name = self.generate_name(author, title)
+            os.rename(pdf_file, os.path.join(self.folder_path, new_name))
 
 
 if __name__ == '__main__':
-    path = 'reportlab-sample.pdf'
-    get_info(path)
+    parser = argparse.ArgumentParser(description='Path to the folder with PDFs.')
+    parser.add_argument('path', help='path to the folder with PDFs that will be renamed')
+
+    args = parser.parse_args()
+
+    reader = PdfReader(args.path)
+    reader.rename_files()
+
