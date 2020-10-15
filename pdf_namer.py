a1eb5477dced7054756d8f7cd55b887594bacd72
svarnypetr <svarnypetr@gmail.com>
2020-10-15 17:52:58 +0200

    create basic namer structure and needed packages

---
+++ pdf_namer.py
@@ -0,0 +1,27 @@
+# get_doc_info.py
+
+from PyPDF2 import PdfFileReader
+
+class PdfReader:
+
+
+
+
+def get_info(path):
+    with open(path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        info = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+
+    print(info)
+
+    author = info.author
+    creator = info.creator
+    producer = info.producer
+    subject = info.subject
+    title = info.title
+
+
+if __name__ == '__main__':
+    path = 'reportlab-sample.pdf'
+    get_info(path)
